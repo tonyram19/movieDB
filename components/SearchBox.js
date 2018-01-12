80ff08d86bebbddf7d8cde4de4488ca4fddb8155
Tony Ramirez <tonyram20@gmail.com>
2018-01-13 00:13:39 +0200

    Folder renaming

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -9,7 +9,6 @@ export default class SearchBox extends Component {
         this.url = 'https://api.themoviedb.org/3';
         this.key = '?api_key=680741ed1242043318d4ce87e825296f';
 
-        this.searchMovieDummy();
     }
 
     searchMovie = () => {
@@ -29,7 +28,7 @@ export default class SearchBox extends Component {
     }
 
     searchMovieDummy = () => {
-        let query = 'batman';
+        let query = 'star wars';
 
         return axios.get(this.url + '/search/movie' + this.key + '&language=en-US&page=1&query=' + query)
             .then(response => {
